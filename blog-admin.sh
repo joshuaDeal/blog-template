2fafd6ae3323ef6cea812c26115b8b4f5d023480
joshuaDeal <thejdeal@tutanota.com>
2025-11-19 09:52:00 -0500

    Include article contents in the rss feed

--- blog-admin.sh
+++ blog-admin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+ 
 # Administrative tools for blog.
 # Add and remove articles and authors. Browse, approve and delete comments. See pending comments. Update rss feed.
-
+ 
 DATABASE="/opt/blog/blog.db"
-
+ 
 newArticleMode=false
 removeArticleMode=false
 removeCommentMode=false
@@ -14,7 +14,7 @@ listPendingCommentsMode=false
 updateRssMode=false
 addAuthorMode=false
 removeAuthorMode=false
-
+ 
 # Print help message
 printHelp() {
 	echo "Usage: $0 [OPTIONS]"
@@ -42,112 +42,112 @@ printHelp() {
 	echo "  -ai, --author-image <filepath> 	Image for author to add."
 	echo "  -rA, --remove-author <name>		Name of author to remove."
 }
-
+ 
 # Parse arguments.
 evalArgs() {
 	arguments=("$@")
-
+ 
 	for ((i = 0; i < ${#arguments[@]}; i++)); do
 		# Print help message.
 		if [ "${arguments[i]}" == "-h" ] || [ "${arguments[i]}" == "--help" ]; then
 			printHelp
 			exit
 		fi
-	
+ 
 		# Get article name.
 		if [ "${arguments[i]}" == "-n" ] || [ "${arguments[i]}" == "--name" ]; then
 			articleName=${arguments[i + 1]}
 			newArticleMode=true
 		fi
-
+ 
 		# Get article title.
 		if [ "${arguments[i]}" == "-T" ] || [ "${arguments[i]}" == "--title" ]; then
 			articleTitle=${arguments[i + 1]}
 			newArticleMode=true
 		fi
-
+ 
 		# Get article author.
 		if [ "${arguments[i]}" == "-a" ] || [ "${arguments[i]}" == "--author" ]; then
 			author=${arguments[i + 1]}
 			newArticleMode=true
 		fi
-
+ 
 		# Get article description.
 		if [ "${arguments[i]}" == "-d" ] || [ "${arguments[i]}" == "--description" ]; then
 			description=${arguments[i + 1]}
 			newArticleMode=true
 		fi
-
+ 
 		# Get article file path.
 		if [ "${arguments[i]}" == "-f" ] || [ "${arguments[i]}" == "--file" ]; then
 			articleFile=${arguments[i + 1]}
 			newArticleMode=true
 		fi
-
+ 
 		# Get article tags.
 		if [ "${arguments[i]}" == "-t" ] || [ "${arguments[i]}" == "--tags" ]; then
 			articleTags=${arguments[i + 1]}
 			newArticleMode=true
 		fi
-
+ 
 		# Get article image path.
 		if [ "${arguments[i]}" == "-i" ] || [ "${arguments[i]}" == "--image" ]; then
 			articleImage=${arguments[i + 1]}
 			newArticleMode=true
 		fi
-
+ 
 		# Remove article.
 		if [ "${arguments[i]}" == "-ra" ] || [ "${arguments[i]}" == "--remove-article" ]; then
 			articleToRemove=${arguments[i + 1]}
 			removeArticleMode=true
 		fi
-
+ 
 		# List comments for specific article.
 		if [ "${arguments[i]}" == "-lc" ] || [ "${arguments[i]}" == "--list-comments" ]; then
 			article=${arguments[i + 1]}
 			listCommentsMode=true
 		fi
-
+ 
 		# Remove comment.
 		if [ "${arguments[i]}" == "-rc" ] || [ "${arguments[i]}" == "--remove-comment" ]; then
 			commentToRemove=${arguments[i + 1]}
 			removeCommentMode=true
 		fi
-
+ 
 		# Toggle comment approval.
 		if [ "${arguments[i]}" == "-ac" ] || [ "${arguments[i]}" == "--approve" ]; then
 			commentToApprove=${arguments[i + 1]}
 			approveCommentMode=true
 		fi
-
+ 
 		# List pending comments.
 		if [ "${arguments[i]}" == "-lp" ] || [ "${arguments[i]}" == "--list-pending" ]; then
 			listPendingCommentsMode=true
 		fi
-
+ 
 		# List pending comments.
 		if [ "${arguments[i]}" == "-ur" ] || [ "${arguments[i]}" == "--update-rss" ]; then
 			updateRssMode=true
 		fi
-
+ 
 		# Add author.
 		if [ "${arguments[i]}" == "-aa" ] || [ "${arguments[i]}" == "--add-author" ]; then
 			authorName=${arguments[i + 1]}
 			addAuthorMode=true
 		fi
-
+ 
 		# Get author bio.
 		if [ "${arguments[i]}" == "-ab" ] || [ "${arguments[i]}" == "--author-bio" ]; then
 			authorBio=${arguments[i + 1]}
 			addAuthorMode=true
 		fi
-
+ 
 		# Get author image.
 		if [ "${arguments[i]}" == "-ai" ] || [ "${arguments[i]}" == "--author-image" ]; then
 			authorImage=${arguments[i + 1]}
 			addAuthorMode=true
 		fi
-
+ 
 		# remove author.
 		if [ "${arguments[i]}" == "-rA" ] || [ "${arguments[i]}" == "--remove-author" ]; then
 			authorName=${arguments[i + 1]}
@@ -155,53 +155,53 @@ evalArgs() {
 		fi
 	done
 }
-
+ 
 # Make sure only one mode is active.
 verifyMode() {
 	modeCount=0
-
+ 
 	if $removeArticleMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $newArticleMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $listCommentsMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $removeCommentMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $approveCommentMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $listPendingCommentsMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $updateRssMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $addAuthorMode; then
 		((modeCount++))
 	fi
-
+ 
 	if $removeAuthorMode; then
 		((modeCount++))
 	fi
-
+ 
 	if [ "$modeCount" -ge 2 ]; then
 		printf "Error: Too many operations at once.\n" >&2
 		exit
 	fi
 }
-
+ 
 # Add new article to sqlite articles table.
 addNewArticle() {
 	# Check that required variables are set.
@@ -210,68 +210,68 @@ addNewArticle() {
 		printf "Error: No article name provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$articleTitle" ]; then
 		printf "Error: No article title provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$author" ]; then
 		printf "Error: No author provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$description" ]; then
 		printf "Error: No article description provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$articleFile" ]; then
 		printf "Error: No article file path provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$articleTags" ]; then
 		printf "Error: No article tags provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$articleImage" ]; then
 		printf "Error: No article image provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ "$missingVars" == true ]; then
 		exit
 	fi
-
+ 
 	# Generate unique article id.
 	exists=1
 	while [ "$exists" -ne 0 ]; do
 		articleId="a-"
-	
+ 
 		for i in {1..10}; do
 			digit=$(( RANDOM % 10 ))
 			articleId+="$digit"
 		done
-	
+ 
 		# Check that article id is unique.
 		exists=$(sqlite3 "$DATABASE" "SELECT COUNT(*) FROM articles WHERE article_id = '$articleId';")
 	done
-
+ 
 	# Check that article name is unique.
 	exists=$(sqlite3 "$DATABASE" "SELECT COUNT(*) FROM articles WHERE name = '$articleName';")
-
+ 
 	if [ "$exists" -ne 0 ]; then
 		printf "Error: Article Name already in use.\n" >&2
 		exit
 	fi
-
+ 
 	# Add article to table.
 	sqlite3 "$DATABASE" "INSERT INTO articles (article_id, title, name, author, description, file, tags, image) VALUES ('$articleId', '$articleTitle', '$articleName', '$author', '$description', '$articleFile', '$articleTags', '$articleImage')"
 	printf "Added '$articleTitle' to database.\nNow is probably a good time to go online and check if it's working.\nAfter that, consider updating the RSS feed.\n"
 }
-
+ 
 removeArticle() {
 	# Check if article exists.
 	exists=$(sqlite3 "$DATABASE" "SELECT COUNT(*) FROM articles WHERE name = '$articleToRemove';")
@@ -279,12 +279,12 @@ removeArticle() {
 		printf "Error: Article dose not exist.\n" >&2
 		exit
 	fi
-
+ 
 	# Remove article.
 	sqlite3 "$DATABASE" "DELETE FROM articles WHERE name = '$articleToRemove'"
 	echo "Removed article '$articleToRemove'"
 }
-
+ 
 # List comments from a specific article.
 listComments() {
 	# Check if article exists.
@@ -293,18 +293,18 @@ listComments() {
 		printf "Error: Article dose not exist.\n" >&2
 		exit
 	fi
-
+ 
 	# Find article id from name.
 	articleId=$(sqlite3 "$DATABASE" "SELECT article_id FROM articles WHERE name = '$article';")
-
+ 
 	# List comments with matching context id.
 	output=$(sqlite3 "$DATABASE" "SELECT comment_id, name, email, comment, ip_address, visible, created_at FROM comments WHERE context_id = '$articleId';")
-
+ 
 	# Print output.
 	echo "comment-id|name|email|comment|ip|approved|date"
 	echo "$output"
 }
-
+ 
 removeComment() {
 	# Check if comment exists.
 	exists=$(sqlite3 "$DATABASE" "SELECT COUNT(*) FROM comments WHERE comment_id = '$commentToRemove';")
@@ -312,12 +312,12 @@ removeComment() {
 		printf "Error: Comment dose not exist.\n" >&2
 		exit
 	fi
-
+ 
 	# Remove comment.
 	sqlite3 "$DATABASE" "DELETE FROM comments WHERE comment_id = '$commentToRemove'"
 	echo "Removed comment '$commentToRemove'"
 }
-
+ 
 toggleCommentApproval() {
 	# Check if comment exists.
 	exists=$(sqlite3 "$DATABASE" "SELECT COUNT(*) FROM comments WHERE comment_id = '$commentToApprove';")
@@ -325,10 +325,10 @@ toggleCommentApproval() {
 		printf "Error: Comment dose not exist.\n" >&2
 		exit
 	fi
-
+ 
 	# Check comment visibility.
 	visible=$(sqlite3 "$DATABASE" "SELECT visible FROM comments WHERE comment_id = '$commentToApprove'")
-
+ 
 	# Change comment visibility.
 	if [ "$visible" == true ]; then
 		sqlite3 "$DATABASE" "UPDATE comments SET visible = 'false' WHERE comment_id = '$commentToApprove';"
@@ -336,56 +336,57 @@ toggleCommentApproval() {
 	elif [ "$visible" == false ]; then
 		sqlite3 "$DATABASE" "UPDATE comments SET visible = 'true' WHERE comment_id = '$commentToApprove';"
 		echo "$commentToApprove visible."
-
+ 
 	fi
 }
-
+ 
 listPendingComments() {
 	# List comments with visibility set to false.
 	output=$(sqlite3 "$DATABASE" "SELECT context_id, comment_id, name, email, comment, ip_address, created_at FROM comments WHERE visible = 'false' ORDER BY created_at ASC;")
-
+ 
 	echo "comment-id|article|name|email|comment|ip|date"
-
+ 
 	while IFS= read -r line; do
 		contextId=$(echo $line | cut -d '|' -f 1)
-
+ 
 		# Resolve context_id to article name.
 		articleName=$(sqlite3 "$DATABASE" "SELECT name FROM articles WHERE article_id = '$contextId'")
-
+ 
 		# Print output
 		echo "$line" | awk -v article="$articleName" 'BEGIN{FS=OFS="|"} {print $2, article, $3, $4, $5, $6, $7}'
 	done <<< $output
 }
-
+ 
 updateRss() {
 	# RSS feed headers
 	printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-	printf "<rss version=\"2.0\">\n"
+	printf "<rss version=\"2.0\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\">\n"
 	printf "\t<channel>\n"
 	printf "\t\t<title>Blog</title>\n"
 	printf "\t\t<link>http://test.example.com</link>\n"
 	printf "\t\t<description>RSS feed for blog.</description>\n"
 	printf "\t\t<language>en-us</language>\n"
-
+ 
 	# Fetch articles from the SQLite database
-	output=$(sqlite3 "$DATABASE" "SELECT title, name, description, author, date FROM articles ORDER BY date DESC")
-
-	echo "$output" | while IFS='|' read -r title name description author date; do
+	output=$(sqlite3 "$DATABASE" "SELECT title, name, description, author, date, file FROM articles ORDER BY date DESC")
+ 
+	echo "$output" | while IFS='|' read -r title name description author date file; do
 		# Print each article in RSS format
 		printf "\t\t<item>\n"
 		printf "\t\t\t<title><![CDATA[$title]]></title>\n"
 		printf "\t\t\t<link>http://test.example.com/article.php?article=$name</link>\n"
 		printf "\t\t\t<description><![CDATA[$description]]></description>\n"
-		printf "\t\t\t<author>$author</author>\n"
+		printf "\t\t\t<author><![CDATA[$author]]></author>\n"
 		printf "\t\t\t<pubDate>$(date -u -d "$date" +"%a, %d %b %Y %H:%M:%S +0000")</pubDate>\n"
+		printf "\t\t\t<content:encoded><![CDATA[$(sed 's/]]>/]]]]><![CDATA[>/' "$file")]]></content:encoded>\n"
 		printf "\t\t</item>\n"
 	done
-
+ 
 	# Close channel
 	printf "\t</channel>\n"
 	printf "</rss>\n"
 }
-
+ 
 addAuthor() {
 	# Check that required variables are set.
 	missingVars=false
@@ -393,26 +394,26 @@ addAuthor() {
 		printf "Error: No author name provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$authorBio" ]; then
 		printf "Error: No author bio provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ -z "$authorImage" ]; then
 		printf "Error: No author image provided.\n" >&2
 		missingVars=true
 	fi
-
+ 
 	if [ "$missingVars" == true ]; then
 		exit
 	fi
-
+ 
 	# Add new author.
 	sqlite3 "$DATABASE" "INSERT INTO authors (name, bio, image) VALUES ('$authorName', '$authorBio', '$authorImage')"
 	echo "Added author '$authorName'"
 }
-
+ 
 removeAuthor() {
 	# Check if author exists.
 	exists=$(sqlite3 "$DATABASE" "SELECT COUNT(*) FROM authors WHERE name =  '$authorName';")
@@ -420,46 +421,46 @@ removeAuthor() {
 		printf "Error: Author dose not exist.\n" >&2
 		exit
 	fi
-
+ 
 	# Remove comment.
 	sqlite3 "$DATABASE" "DELETE FROM authors WHERE name = '$authorName';"
 	echo "Removed author '$authorName'"
 }
-
+ 
 evalArgs "$@"
 verifyMode
 if [ "$newArticleMode" == true ]; then
 	addNewArticle
 fi
-
+ 
 if [ "$removeArticleMode" == true ]; then
 	removeArticle
 fi
-
+ 
 if [ "$listCommentsMode" == true ]; then
 	listComments
 fi
-
+ 
 if [ "$removeCommentMode" == true ]; then
 	removeComment
 fi
-
+ 
 if [ "$approveCommentMode" == true ]; then
 	toggleCommentApproval
 fi
-
+ 
 if [ "$listPendingCommentsMode" == true ]; then
 	listPendingComments
 fi
-
+ 
 if [ "$updateRssMode" == true ]; then
 	updateRss
 fi
-
+ 
 if [ "$addAuthorMode" == true ]; then
 	addAuthor
 fi
-
+ 
 if [ "$removeAuthorMode" == true ]; then
 	removeAuthor
 fi
